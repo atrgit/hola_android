8780723c436f1ac985c9f652c4dda81bcfb6389f
Alejandro Talavera Rosales <badboy@unam.mx>
2016-05-28 11:45:12 -0500

    Primera verision

---
+++ app/src/main/java/docencia/tic/unam/mx/hola2/MainActivity.java
@@ -0,0 +1,13 @@
+package docencia.tic.unam.mx.hola2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
